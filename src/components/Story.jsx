4db17511b151002ee9bd737e0ecfa3d02dd46b9f
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-02-15 00:09:03 +0330

    Create Story.jsx

---
+++ src/components/Story.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Story = () => {
+  return (
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
