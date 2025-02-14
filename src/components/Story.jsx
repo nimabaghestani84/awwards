375036503bf2cb340c17bb2de6abac961f603db6
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-02-15 00:59:55 +0330

    end

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
 import gsap from "gsap";
+import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
 
 const Story = () => {
-    const frameRef = useRef(null);
+  const frameRef = useRef(null);
 
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -43,7 +45,7 @@ const Story = () => {
     }
   };
   return (
-    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
           the multiversal ip world
@@ -59,21 +61,39 @@ const Story = () => {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
-                ref={frameRef}
-                onMouseLeave={handleMouseLeave}
-                onMouseUp={handleMouseLeave}
-                onMouseEnter={handleMouseLeave}
-                onMouseMove={handleMouseMove}
-                  src="../../public/img/entrance.webp"
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/entrance.webp"
                   alt="entrance.webp"
                   className="object-contain"
                 />
               </div>
             </div>
+
+            <RoundedCorners/>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
